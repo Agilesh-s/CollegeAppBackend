6c59a9dd9a4b3657c86f07d049f4e6b0f07fddca
Agilesh-s <agileshsivakumar.s@gmail.com>
2022-10-19 10:10:11 +0530

    Initial Commit

---
+++ src/main/java/com/example/xyzCollegeApp/XyzCollegeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.xyzCollegeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzCollegeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzCollegeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xyzCollegeApp/XyzCollegeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xyzCollegeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzCollegeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
